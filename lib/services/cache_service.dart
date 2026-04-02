25cdadd0c07bf0aa8e15e55efcc15d234c3b5c0a
Aleksei <Chegaev>
2026-04-02 22:06:55 +0300

    drift schema added and cache service created

---
+++ lib/services/cache_service.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
+
+class CacheService {
+  final _uuid = const Uuid();
+
+  Future<(String id, String path, int totalWords)> cacheBook(
+    List<String> words,
+  ) async {
+    final id = _uuid.v4();
+
+    final dir = await getApplicationDocumentsDirectory();
+    final cacheDir = Directory('${dir.path}/cache');
+
+    if (!cacheDir.existsSync()) {
+      cacheDir.createSync(recursive: true);
+    }
+
+    final path = '${cacheDir.path}/$id.json';
+
+    final file = File(path);
+    await file.writeAsString(jsonEncode(words));
+
+    return (id, path, words.length);
+  }
+
+  Future<List<String>> loadBook(String id) async {
+  final dir = await getApplicationDocumentsDirectory();
+  final path = '${dir.path}/cache/$id.json';
+
+  final file = File(path);
+
+  if (!file.existsSync()) {
+    throw Exception('Book file not found');
+  }
+
+  final content = await file.readAsString();
+
+  final List<dynamic> decoded = jsonDecode(content) as List;
+
+  final List<String> words = decoded.map((e) => e.toString()).toList();
+
+  return words;
+  }
+
+}
--- lib/services/text_processor.dart
+++ lib/services/text_processor.dart
@@ -4,7 +4,13 @@ class TextProcessor {
   List<RsvpToken> process(String rawText) {
     final List<RsvpToken> result = [];
 
-    final splittedText = rawText.replaceAll('\n', ' ').replaceAll(RegExp(r'\s+'), ' ').trim().split(' ');
+    final splittedText = rawText
+        .replaceAll('\n', ' ')
+        .replaceAll(',', '')
+        .replaceAll('.', '')
+        .replaceAll(RegExp(r'\s+'), ' ')
+        .trim()
+        .split(' ');
     for (int i = 0; i < splittedText.length; i++) {
       result.add(RsvpToken(text: splittedText[i], index: i));
     }
