c358fdcd08f923ddde106154e10eaf110f8fa173
Ivan Chabanov <firstchabanov@gmail.com>
2026-04-02 21:22:21 +0300

    Linter nit

--- lib/features/rsvp_engine/presentation/ui/pages/main_page.dart
+++ lib/features/rsvp_engine/presentation/ui/pages/main_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rsvp_flutter_app/core/di/di.dart';
-import 'package:rsvp_flutter_app/features/file_picking/domain/usecases/import_book_file.dart';
 import 'package:rsvp_flutter_app/features/rsvp_engine/presentation/state/bloc/rsvp_bloc.dart';
 import 'package:rsvp_flutter_app/features/rsvp_engine/presentation/ui/widgets/new_book_button.dart';
 
@@ -21,13 +20,11 @@ class _MainScreenState extends State<MainScreen> {
         appBar: AppBar(
           title: const Text('TikTok-book'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
+        body: const Padding(
+          padding: EdgeInsets.all(8.0),
           child: Center(
             child: Column(
-              children: [
-                NewBookButton(),
-              ],
+              children: [NewBookButton()],
             ),
           ),
         ),
