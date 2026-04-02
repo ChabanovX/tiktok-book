b83fcd0cdb8f98fbf6cc37db23fe13781fc0c4a4
Arsen Latipov <latipovarsen2006@gmail.com>
2026-04-02 23:02:46 +0300

    fix ci/cd

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,18 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-<<<<<<< HEAD
 #include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   Sqlite3FlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
-=======
-#include <url_launcher_windows/url_launcher_windows.h>
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
->>>>>>> ec15a21 (add start stop button)
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,10 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-<<<<<<< HEAD
   sqlite3_flutter_libs
-=======
->>>>>>> ec15a21 (add start stop button)
   url_launcher_windows
 )
 
