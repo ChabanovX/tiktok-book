d3da36faec85069c8e2e77afa3766a166bf9058c
Ivan Chabanov <firstchabanov@gmail.com>
2026-04-04 00:39:16 +0300

    Update `buildRunner`

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -21,6 +21,13 @@ import 'package:rsvp_flutter_app/features/file_picking/domain/repositories/file_
     as _i69;
 import 'package:rsvp_flutter_app/features/file_picking/domain/usecases/import_book_file.dart'
     as _i749;
+import 'package:rsvp_flutter_app/features/rsvp_engine/presentation/state/bloc/rsvp_bloc.dart'
+    as _i422;
+import 'package:rsvp_flutter_app/services/book_converter.dart' as _i216;
+import 'package:rsvp_flutter_app/services/cache_service.dart' as _i332;
+import 'package:rsvp_flutter_app/services/pdf_parser.dart' as _i500;
+import 'package:rsvp_flutter_app/services/text_processor.dart' as _i1019;
+import 'package:rsvp_flutter_app/services/txt_parser.dart' as _i91;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -31,11 +38,28 @@ extension GetItInjectableX on _i174.GetIt {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
     gh.singleton<_i51.FilePickerDataSource>(() => _i51.FilePickerDataSource());
     gh.singleton<_i303.FileLoaderService>(() => _i303.FileLoaderService());
+    gh.singleton<_i332.CacheService>(() => _i332.CacheService());
+    gh.lazySingleton<_i500.PdfParser>(() => _i500.PdfParser());
+    gh.lazySingleton<_i1019.TextProcessor>(() => _i1019.TextProcessor());
+    gh.lazySingleton<_i91.TxtParser>(() => _i91.TxtParser());
     gh.lazySingleton<_i69.FileRepository>(
       () => _i542.FileRepositoryImpl(gh<_i303.FileLoaderService>()),
     );
     gh.singleton<_i749.ImportBookFile>(
       () => _i749.ImportBookFile(gh<_i69.FileRepository>()),
+    );
+    gh.singleton<_i216.BookConverter>(
+      () => _i216.BookConverter(
+        gh<_i500.PdfParser>(),
+        gh<_i91.TxtParser>(),
+        gh<_i1019.TextProcessor>(),
+      ),
+    );
+    gh.factory<_i422.RsvpBloc>(
+      () => _i422.RsvpBloc(
+        fileRepository: gh<_i69.FileRepository>(),
+        bookConverter: gh<_i216.BookConverter>(),
+      ),
     );
     return this;
   }
