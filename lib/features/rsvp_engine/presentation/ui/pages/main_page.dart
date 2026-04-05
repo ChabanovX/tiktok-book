8193d360845c67ffdcd7eaf63ceab839af9ead08
Ivan Chabanov <firstchabanov@gmail.com>
2026-04-06 00:01:44 +0300

    Remove unused code in `MainScreen`

--- lib/features/rsvp_engine/presentation/ui/pages/main_page.dart
+++ lib/features/rsvp_engine/presentation/ui/pages/main_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rsvp_flutter_app/core/di/di.dart';
 import 'package:rsvp_flutter_app/features/rsvp_engine/presentation/state/bloc/rsvp_bloc.dart';
-import 'package:rsvp_flutter_app/features/rsvp_engine/presentation/ui/widgets/mock_reading_button.dart';
 import 'package:rsvp_flutter_app/features/rsvp_engine/presentation/ui/widgets/new_book_button.dart';
 import 'package:rsvp_flutter_app/features/ui_kit/ui_kit.dart';
 
@@ -11,28 +10,6 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const state = LibraryMainScreenState.nonEmpty;
-    const bookItems = [
-      BookItem(
-        title: 'Project Hail Mary',
-        author: 'Andy Weir',
-        progress: 0.18,
-        icon: Icons.rocket_launch_outlined,
-      ),
-      BookItem(
-        title: 'Atomic Habits',
-        author: 'James Clear',
-        progress: 0.62,
-        icon: Icons.psychology_alt_outlined,
-      ),
-      BookItem(
-        title: 'Dune',
-        author: 'Frank Herbert',
-        progress: 1,
-        icon: Icons.auto_stories_outlined,
-      ),
-    ];
-
     return BlocProvider(
       create: (context) => getIt<RsvpBloc>(),
       child: BlocBuilder<RsvpBloc, RsvpState>(
