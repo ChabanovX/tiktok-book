3cd9b9dd6acae90294e82609136e8aae17700ea9
Ivan Chabanov <firstchabanov@gmail.com>
2026-04-07 12:56:48 +0300

    Remove book from event lol (unnecessary)

--- lib/features/rsvp_engine/presentation/state/bloc/rsvp_event.dart
+++ lib/features/rsvp_engine/presentation/state/bloc/rsvp_event.dart
@@ -3,7 +3,7 @@ part of 'rsvp_bloc.dart';
 @freezed
 class RsvpEvent with _$RsvpEvent {
   const factory RsvpEvent.started() = _Started;
-  const factory RsvpEvent.addBook(BookMetaModel bookMetaModel) = _AddBook;
+  const factory RsvpEvent.addBook() = _AddBook;
   const factory RsvpEvent.removeBook({required BookMetaModel book}) = _RemoveBook;
   const factory RsvpEvent.toggleSelectBook({required BookMetaModel book}) = _ToggleSelectBook;
   const factory RsvpEvent.startAnimation({required int bookID}) = _StartAnimation;
