84ef1742fc172b1a5589abfa15c7b69c2ae4138a
Ivan Chabanov <firstchabanov@gmail.com>
2026-04-08 17:15:37 +0300

    Make drift db web-compatable

--- lib/core/db/app_database.dart
+++ lib/core/db/app_database.dart
@@ -1,24 +1,24 @@
-import 'dart:io';
 import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
+import 'package:drift_flutter/drift_flutter.dart';
 import 'package:injectable/injectable.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:rsvp_flutter_app/core/db/tables/books.dart';
+
 part 'app_database.g.dart';
 
 @singleton
 @DriftDatabase(tables: [Books])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase() : super(_openConnection());
+  AppDatabase()
+    : super(
+        driftDatabase(
+          name: 'app',
+          web: DriftWebOptions(
+            sqlite3Wasm: Uri.parse('sqlite3.wasm'),
+            driftWorker: Uri.parse('drift_worker.dart.js'),
+          ),
+        ),
+      );
 
   @override
   int get schemaVersion => 1;
-}
-
-LazyDatabase _openConnection() {
-  return LazyDatabase(() async {
-    final dir = await getApplicationDocumentsDirectory();
-    final file = File('${dir.path}/app.db');
-    return NativeDatabase(file);
-  });
 }
